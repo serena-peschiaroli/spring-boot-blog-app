c031f64b07d0febdd0b4397ab2a28cf871b959d3
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-05-13 20:21:02 +0200

    Add: new post template, PostMapping in post controller

--- src/main/java/com/serpes/springbootblogapp/controllers/PostController.java
+++ src/main/java/com/serpes/springbootblogapp/controllers/PostController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -48,5 +50,11 @@ public class PostController {
                  }else{
                      return "redirect:/";
                  }
+     }
+
+     @PostMapping("/posts/new")
+    public String saveNewPost(@ModelAttribute Post post){
+         postService.save(post);
+         return "redirect:/posts/" + post.getId();
      }
 }
